93fea687361f70fddefe6b24b330f1f7c8b49ed8
Marcos <suporte.ti.marcos@gmail.com>
2024-04-30 09:24:58 -0300

    Adicionado Categories e Owner

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,18 @@
 from typing import Any
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -14,6 +24,15 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/' ) # Indicando pasta e dizendo para criar
                                                                      # pasta /pictures/ano/mes  dentro de /media
+    category = models.ForeignKey(Category, 
+                                 on_delete=models.SET_NULL, #on_delete = diz o que fazer com contact se category 
+                                 blank=True,        # for deletado. Está configurado para setar NULL caso 
+                                 null=True)      #category deletado. Importante o campo estar blank e permitir null
+    owner = models.ForeignKey(User,
+                              on_delete=models.SET_NULL,
+                              blank=True,
+                              null=True)                  
 
     def __str__(self) -> str:          # Metodo implementado para trocar o nome de exibição
         return f'{self.first_name} {self.last_name}'  # No Admin do Django
+
