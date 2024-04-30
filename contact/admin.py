bb1f231642f95276718729f8c172c211ac9c9be7
Marcos <suporte.ti.marcos@gmail.com>
2024-04-30 08:17:42 -0300

    Configurado Imagem, urls static e media

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = ('id',)  # Para ordernar decrescente apenas colocar um - na frente do id
+    # list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name', )
+    list_per_page = 10
+    list_max_show_all = 200
+    # list_editable = ('first_name', 'last_name',)
+    list_display_links = ('id', 'first_name',)
+
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
+from typing import Any
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100, blank=True) # Blank para não ser obrigatório
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now) # Função para gerar data automatica
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/' ) # Indicando pasta e dizendo para criar
+                                                                     # pasta /pictures/ano/mes  dentro de /media
+
+    def __str__(self) -> str:          # Metodo implementado para trocar o nome de exibição
+        return f'{self.first_name} {self.last_name}'  # No Admin do Django
